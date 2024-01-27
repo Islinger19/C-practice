a204842258a322209e73b720590a72eef2a42f39
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-01-27 18:54:07 +0530

    C-Practice

---
+++ goto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    label:
+        printf("we are inside label\n");
+        goto end;
+    printf("Hello World\n");
+    goto label;
+    end:
+        printf("we are at end");
+    return 0;
+}
