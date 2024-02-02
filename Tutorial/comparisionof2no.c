03442d6afdf77c883bd527ce9cd43bf27a06c14c
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-02-02 09:28:34 +0530

    Tutorial

--- Tutorial/comparisionof2no.c
+++ Tutorial/comparisionof2no.c
@@ -6,9 +6,26 @@ int main() {
     scanf("%d", &num1);
     printf("Enter the second number: ");
     scanf("%d", &num2);
-
+/*
     (num1 == num2) ? printf("Both numbers are equal.\n") : (num1 > num2) ? printf("%d is greater than %d.\n", num1, num2) : printf("%d is greater than %d.\n", num2, num1);
+   */
+   switch (num1 > num2)
+   {
+   case 1:  
+    printf("%d is greater than %d.\n",num1,num2);
+    break;
+   case 0:
+    switch (num1<num2)
+    {
+    case 1:
+        printf("%d is greater than %d.\n",num2,num1);
+        break;
     
+    case 0:
+        printf("Both numbers are equal.\n");
+        break;
+    }
+   }
 /*
     if (num1 == num2) {
         printf("Both numbers are equal.\n");
@@ -20,5 +37,6 @@ int main() {
         printf("%d is greater than %d.\n",num2,num1);
     }
 */
+
     return 0;
 }
