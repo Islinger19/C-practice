6c70b5d5b1838ab7a316510570a4ff813a2394da
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-03-29 09:50:16 +0530

    Tutorial

--- Tutorial/stingfunc.c
+++ Tutorial/stingfunc.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     char str[100];
     int length = 0, charCount = 0, vowelCount = 0;
-    char copyStr[100];
+    char copyStr[100] = {"Sanidhya Awasthi"};
 
     printf("Enter a string: ");
     gets(str);
 
-    while (str[length] != 0) {
+    while (str[length] != 0)
+    {
         length++;
     }
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         charCount++;
-
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
-            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
             vowelCount++;
         }
     }
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         copyStr[i] = str[i];
     }
     copyStr[length] = '\0';
