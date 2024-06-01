95dd4b40398654d95c03dae267a87dc2cb683473
Islinger19 <Sanidhya.awasthi@gmail.com>
2024-06-01 15:01:26 +0530

    c practice

---
+++ Tutorial/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+int Add()
+{
+    return A + B;
+}
---
+++ Tutorial/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+
+int A = 2;
+int B = 3;
+int Add()
+{
+    return A + B;
+}
+int main()
+{
+    int answer;
+    int A = 5;
+    int B = 7;
+    answer = Add();
+    printf("%d\n", answer);
+    return 0;
+}
